f5e19fbb62b4cae49e9a08658d823f4af5e4b781
patrick-watsonica <93407869+patrick-watsonica@users.noreply.github.com>
2022-01-18 20:49:04 +0000

    Added template files

---
+++ app.js
@@ -0,0 +1,62 @@
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const navItem1 = document.querySelector('.nav-item1');
+const navItem2 = document.querySelector('.nav-item2');
+
+// when someone clicks the hamburger button
+navToggle.addEventListener('click', () => {
+	const visibility = nav.getAttribute('data-visible');
+	if (visibility === 'false') {
+		nav.setAttribute('data-visible', true);
+		navToggle.setAttribute('aria-expanded', true);
+		document.body.style.overflow = 'hidden';
+	} else {
+		nav.setAttribute('data-visible', false);
+		navToggle.setAttribute('aria-expanded', false);
+		document.body.style.overflow = 'auto';
+	}
+});
+
+navItem1.addEventListener('click', () => {
+	const visibility = nav.getAttribute('data-visible');
+	if (visibility === 'false') {
+		nav.setAttribute('data-visible', true);
+		navToggle.setAttribute('aria-expanded', true);
+	} else {
+		nav.setAttribute('data-visible', false);
+		navToggle.setAttribute('aria-expanded', false);
+	}
+});
+
+navItem2.addEventListener('click', () => {
+	const visibility = nav.getAttribute('data-visible');
+	if (visibility === 'false') {
+		nav.setAttribute('data-visible', true);
+		navToggle.setAttribute('aria-expanded', true);
+	} else {
+		nav.setAttribute('data-visible', false);
+		navToggle.setAttribute('aria-expanded', false);
+	}
+});
+
+navItem3.addEventListener('click', () => {
+	const visibility = nav.getAttribute('data-visible');
+	if (visibility === 'false') {
+		nav.setAttribute('data-visible', true);
+		navToggle.setAttribute('aria-expanded', true);
+	} else {
+		nav.setAttribute('data-visible', false);
+		navToggle.setAttribute('aria-expanded', false);
+	}
+});
+
+navItem4.addEventListener('click', () => {
+	const visibility = nav.getAttribute('data-visible');
+	if (visibility === 'false') {
+		nav.setAttribute('data-visible', true);
+		navToggle.setAttribute('aria-expanded', true);
+	} else {
+		nav.setAttribute('data-visible', false);
+		navToggle.setAttribute('aria-expanded', false);
+	}
+});
